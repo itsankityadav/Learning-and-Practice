d70e8e85bdbf16aef7ee1b2ba552458159a30932
Ankit Yadav <111086183+itsankityadav@users.noreply.github.com>
2023-09-14 05:49:42 +0000

    #add codes

--- Hitesh_Javascript/File_1.js
+++ Hitesh_Javascript/File_1.js
@@ -4,3 +4,12 @@ var crush_name = "Akansha Sonkar";
 location = "Prayagraj";
 
 console.table([accountId, name, crush_name, location])
+console.table([typeof accountId, typeof name, typeof crush_name, typeof location])
+
+// const accountId = 15343;
+// let name = "Ankit";
+var crush_name = "Akansha";
+location = "Bengaluru";
+
+console.table([accountId, name, crush_name, location])
+console.table([typeof accountId, typeof name, typeof crush_name, typeof location])
